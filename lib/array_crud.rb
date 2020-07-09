869f7d2a1a425a1254ad08773bce390fe40b6c9c
Antonio James <asjames18@gmail.com>
2020-07-09 18:57:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ create_an_array = ["wow", "I", "am", "really"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+add_element_to_end_of_array = ["wow", "I", "am", "really", "learning"]
+add_element_to_end_of_array << ("array")
 end
 
 def add_element_to_start_of_array(array, element)
